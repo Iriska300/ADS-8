aa08545f4890419c12de938dfe5f56ec04c73c0f
Iriska300 <kuzyakowa.i-16@yandex.ru>
2026-05-31 20:43:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,46 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+#include <cctype>
+#include <string>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Ошибка: не могу открыть файл " << filename << std::endl;
+        return;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            word.push_back(tolower(ch));
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+    system("mkdir result");
+
+    std::ofstream outFile("result/freq.txt");
+
+    tree.printByFrequency(std::cout);
+    tree.printByFrequency(outFile);
+
+    outFile.close();
 }
